164f5b9cea6a1d9776e01b4d82e20cc5c6db185a
muhammadsubhan971 <subhan.kh753@gmail.com>
2026-04-24 17:56:32 +0500

    Add start.sh to set up Python virtual environment

---
+++ start.sh
@@ -0,0 +1,12 @@
+# remove old env (optional but recommended)
+rmdir /s /q venv
+
+# create new env with Python 3.12
+python3.12 -m venv venv
+venv\Scripts\activate
+
+# upgrade pip
+python -m pip install --upgrade pip
+
+# install deps
+pip install -r requirements.txt
